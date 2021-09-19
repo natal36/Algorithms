521cf43f036bbb6b851137789cdfaf8dd30769a1
Natalia Sykes <natalia.sykes81@outlook.com>
2021-09-19 13:09:52 +0000

    new code

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function addNumbers(num1,num2) {
-    return num1 + num2;
+function addNumbers(num1, num2) {
+  return num1 + num2;
 }
 
-console.log(addNumbers(2,5));
-console.log(addNumbers(3,6));
+console.log(addNumbers(2, 5));
+console.log(addNumbers(3, 6));
+
+
+function convertMinutes(num1) {
+  return num1 * 60;
+}
+console.log(convertMinutes(5));
+console.log(convertMinutes(3));
+
+function yourAgeinSeconds(age) {
+  return age * 365 *24 *60 *60;
+}
+console.log(yourAgeinSeconds(40));
